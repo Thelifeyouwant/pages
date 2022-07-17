a44b647e1133c7946386d41fc4ed42b22c67cd3a
Thelifeyouwant <76838630+Thelifeyouwant@users.noreply.github.com>
2022-07-17 08:39:38 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.okeyccc.cf'
+      url.hostname = ''
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
